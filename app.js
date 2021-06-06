37065b17890cd5bfbd88586f2ab67dc7415733d3
disaisoft <disaisoft@gmail.com>
2021-06-05 22:21:42 -0500

    actualizacion de nombres

---
+++ app.js
@@ -0,0 +1,10 @@
+const names = ['Paola', 'Jenny', 'Susana', 'Rocio', 'Inmaculada'];
+const capitalNames = [];
+
+
+for (let i = 0; i < names.length; i++){
+    const capitalNames = names[i] .toUpperCase();
+    capitalNames.push(capitalNames);
+}
+
+console.log(capitalNames);
